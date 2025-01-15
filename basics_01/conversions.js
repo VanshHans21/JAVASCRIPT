e474fbd5f342010aa5b2e86bfe2af401f13eca60
VanshHans21 <vanshhans21102005@gmail.com>
2025-01-15 20:42:01 +0000

    container configuration done

---
+++ basics_01/conversions.js
@@ -0,0 +1,62 @@
+// let number = 4500 
+//  console.log(number);
+
+//  let into_string = String(number)    
+//  console.log(into_string);            // 4500 -> 4500
+
+//  console.log(typeof into_string)    // for confirmation check the datatype by typeof
+ 
+//  let into_boolean = Boolean(number)
+//  console.log(into_boolean);       // 4500 in boolean is true
+
+//  console.log(typeof into_boolean) 
+
+
+
+
+ // example of strings 
+
+
+ let some_string = ""
+//  let in_boolean = Boolean(some_string)
+//  console.log(in_boolean);        
+
+
+
+
+ //  "3333"  string  in boolean is true
+ // ""     in boolean gives false
+
+
+
+  let in_num = Number(some_string)
+  console.log(in_num);                  
+
+ console.log(typeof in_num) 
+
+
+
+
+
+
+
+// to number :------
+
+
+
+  //   "3333abc" -> NaN
+  //     "" ->  0
+  //   null -> 0
+  //  undefined -> NaN
+  // true -> 1 ; false -> 0
+
+
+
+
+
+//   to Boolean : ------
+
+
+// "33" -> true 
+// ""  -> false
+// "Vansh" -> true
